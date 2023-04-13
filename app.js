8458f5764bb5fb044e8a7246a9e03857f10f8ad6
Jaes98 <marcus.jaes@gmail.com>
2023-04-13 10:03:12 +0200

    masse loops lavet

--- app.js
+++ app.js
@@ -83,13 +83,88 @@ function initApp() {
     console.log("initApp: app.js is running 🎉");
 }
 
+function showObject(object) {
+    if (object === 2023) {
+        console.log("Current year");
+    }
+    else { console.log(object);}
+}
+
 // ========== forEach ========== //
 
-// ========== for loop ========== //
+names.forEach(showObject);
+teachers.forEach(showObject);
+years.forEach(showObject);
+
+// // ========== for loop ========== //
+for (let index = 0; index < names.length; index++) {
+    const name = names[index]
+    console.log(name);
+}
+
+function loopThroughFiveFirstYears() {
+    for (let index = 0; index < years.length; index++) {
+    const element = years[index];
+    if (element === 2023 && element <= 1990) {
+      console.log("Current year");
+    } else if (element <= 1990) {
+      console.log(element);
+    }
+}
+}
+function loopThroughFiveLastYears() {
+  for (let index = 0; index < years.length; index++) {
+    const element = years[index];
+    if (element === 2023 && element >= 1990) {
+      console.log("Current year");
+    } else if (element >= 1990) {
+      console.log(element);
+    }
+  }
+}
+loopThroughFiveFirstYears();
+loopThroughFiveLastYears();
+
+for (let index = 0; index < 10; index++) {
+    const element = array[index];
+    console.log(element);
+}
+
+for (let index = 1; index <= 10; index++) {
+    const element = array[index];
+    console.log(element);
+}
+for (let index = 10; index <= 0; index--) {
+    const element = array[index];
+    if (element === 0) {
+        console.log("Liftoff!");
+    }
+    else {
+        console.log(element);
+    }
+}
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
 
 // ========== for of loop ========== //
 
+for (const index of years) {
+    if (index === 2023) {
+        console.log("Current year");
+    }
+    else { console.log(index);} 
+}
+
 // ========== for in loop ========== //
+
+for (const index in names) {
+    if (names.hasOwnProperty.call(names, index)) {
+        const element = names[index];
+        console.log(element);
+    }
+}
 
 // ========== while loops ========== //
 
